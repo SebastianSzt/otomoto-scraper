aeb6d0fa1d23cac05cf4a4259051032c07eb840d
Sebastian Sztankowski <sebastiansztankowski@wp.pl>
2024-08-09 11:25:26 +0200

    Add csv generator

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/Scraper'
 require_relative 'lib/Car'
+require_relative 'lib/CSVGenerator'
 
 base_url = 'https://www.otomoto.pl/osobowe/audi/a6'
 number_of_pages = 2
@@ -9,6 +10,9 @@ begin
   cars = scraper.scrape_pages
 
   cars.each(&:display)
+
+  csv_writer = CarCSVWriter.new('cars.csv')
+  csv_writer.write_to_csv(cars)
 rescue StandardError => e
   puts "An error occurred: #{e.message}"
 end
